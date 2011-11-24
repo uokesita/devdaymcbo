aded73324bc60a224253f6fe8f38a0b427ce5479
uokesita <osledybazo@gmail.com>
2011-11-24 19:18:10 -0430

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Devdaymcbo::Application.config.secret_token = 'fb1c589bd1a4a66c88b1957a48b77e3b3f26edd84b220e6fda65546bea1162860e4027d063faa3e296edfbd32b1180108fa9d90b9b2cae5a486b286140021e76'
